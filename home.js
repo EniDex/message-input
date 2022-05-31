5e4680567baf5a1c1e9716868f8fe111164cc9ab
obirije destiny <obirijedestiny@gmail.com>
2022-05-31 16:02:21 +0100

    new files

---
+++ home.js
@@ -0,0 +1,11 @@
+const Form=document.querySelector(".form");
+const Message=document.querySelector(".text");
+const Btn=document.querySelector(".btn");
+const value=document.querySelector(".value");
+const messageContent=document.querySelector(".message-content");
+
+Btn.addEventListener("click",function(){
+    
+})
+
+
